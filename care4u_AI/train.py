27387fb7318a06322eca102f0bf8f00cc8e84cb4
hackma hackma <mauricio.jimenez@globant.com>
2019-09-13 18:37:32 -0500

    commit initial

---
+++ care4u_AI/train.py
@@ -0,0 +1,169 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch import optim
+from torch.autograd import Variable
+from torchvision import transforms, utils, datasets, models
+import io
+from PIL import Image
+import warnings
+warnings.filterwarnings("ignore")
+
+np.random.seed(45)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+#model = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True)
+model = models.resnet50(pretrained=True)
+
+def load_split_train_test(datadir, valid_size = .2):
+    train_transforms = transforms.Compose([transforms.Resize(224),
+                                         transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                   std=[0.229, 0.224, 0.225])
+                                       ])
+    test_transforms = transforms.Compose([transforms.Resize(224),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])
+                                      ])
+    train_data = datasets.ImageFolder(datadir,       
+                    transform=train_transforms)
+    test_data = datasets.ImageFolder(datadir,
+                    transform=test_transforms)
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(train_data,
+                   sampler=train_sampler, batch_size=64)
+    testloader = torch.utils.data.DataLoader(test_data,
+                   sampler=test_sampler, batch_size=64)
+    return trainloader, testloader
+
+trainloader, testloader = load_split_train_test('root', .2)
+print(trainloader.dataset.classes)
+
+for param in model.parameters():
+    param.requires_grad = False
+    
+model.fc = nn.Sequential(nn.Linear(2048, 512),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(512, 10),
+                                 nn.LogSoftmax(dim=1))
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+model.to(device)
+
+epochs = 1
+steps = 0
+running_loss = 0
+print_every = 10
+train_losses, test_losses = [], []
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in testloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    test_loss += batch_loss.item()
+                    
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+            train_losses.append(running_loss/len(trainloader))
+            test_losses.append(test_loss/len(testloader))                    
+            print(f"Epoch {epoch+1}/{epochs}.. " \
+                  f"Train loss: {running_loss/print_every:.3f}.. " \
+                  f"Test loss: {test_loss/len(testloader):.3f}.. " \
+                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+            running_loss = 0
+            model.train()
+torch.save(model, 'care4u.pth')
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
+plt.show()
+
+
+
+classes = ['chips', 'coke', 'eggs']
+test_transforms = transforms.Compose([transforms.Resize(224),
+                                         transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                   std=[0.229, 0.224, 0.225])
+                                       ])
+model=torch.load('care4u.pth')
+model.eval()
+
+def transform_image(image_bytes):
+    my_transforms = test_transforms
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+    
+with open("chips2.jpg", 'rb') as f:
+    image_bytes = f.read()
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    print(classes[y_hat.item()])
+
+
+
+'''
+transform = transforms.Compose([transforms.Resize(256),
+                               transforms.CenterCrop(224),
+                               #transforms.RandomRotation(45), 
+                               #transforms.RandomCrop(256),
+                               #transforms.RandomHorizontalFlip(),
+                               #transforms.RandomResizedCrop(224),
+                               transforms.ToTensor(),
+                               transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                    std=[0.229, 0.224, 0.225])])
+
+snack_food = datasets.ImageFolder('root', transform)
+snack_food_dataset_loader = DataLoader(snack_food, batch_size=10, shuffle=True,  num_workers=5)
+
+n_class = 3
+
+# model
+model = torch.hub.load('pytorch/vision', 'resnet18', pretrained=True)
+num_features = model.fc.in_features
+model.fc = nn.Linear(num_features, n_class)
+
+#print([name, name2 in for name, child in model.named_children() for name2, params in child.named_parameters()])
+print(model)
+
+
+model.eval()
+
+#with torch.no_grad():
+#    output = model(input_batch)
+'''
+
